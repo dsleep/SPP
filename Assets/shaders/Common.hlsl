3ed837acedae07edfe19adf25e60c648910f169c
David Sleeper <david.sleeper@gmail.com>
2021-08-10 12:11:36 -0400

    - new shaders

---
+++ Assets/shaders/Common.hlsl
@@ -0,0 +1,52 @@
+#define MESH_SIG \
+	"RootFlags(ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT | DENY_GEOMETRY_SHADER_ROOT_ACCESS), " \
+		"CBV(b0, visibility=SHADER_VISIBILITY_ALL )," \
+		"CBV(b1, space = 0, visibility = SHADER_VISIBILITY_ALL )," \
+		"CBV(b1, space = 1, visibility = SHADER_VISIBILITY_PIXEL ), " \
+		"CBV(b2, space = 1, visibility = SHADER_VISIBILITY_PIXEL )," \
+		"CBV(b1, space = 2, visibility = SHADER_VISIBILITY_DOMAIN ), " \
+		"CBV(b1, space = 3, visibility = SHADER_VISIBILITY_MESH), " \
+		"RootConstants(b3, num32bitconstants=5), " \
+		"DescriptorTable( SRV(t0, space = 0, numDescriptors = unbounded, flags = DESCRIPTORS_VOLATILE) )," \
+		"DescriptorTable( SRV(t0, space = 1, numDescriptors = unbounded, flags = DESCRIPTORS_VOLATILE) )," \
+		"DescriptorTable( SRV(t0, space = 2, numDescriptors = unbounded, flags = DESCRIPTORS_VOLATILE) )," \
+		"DescriptorTable( SRV(t0, space = 3, numDescriptors = unbounded, flags = DESCRIPTORS_VOLATILE) )," \
+		"DescriptorTable( SRV(t0, space = 4, numDescriptors = unbounded, flags = DESCRIPTORS_VOLATILE) )," \
+		"DescriptorTable( Sampler(s0, numDescriptors = 32, flags = DESCRIPTORS_VOLATILE) )"
+
+static const float PI = 3.141592;
+static const float Epsilon = 0.00001;
+
+struct _ViewConstants 
+{
+	//all origin centered
+	float4x4 ViewMatrix;
+	float4x4 ViewProjectionMatrix;
+	float4x4 InvViewProjectionMatrix;
+	//real view position
+	double3 ViewPosition;
+	//origin centered 
+	double4 CameraFrustum[6];
+
+	float RecipTanHalfFovy;
+};
+
+struct _DrawConstants
+{
+	//altered viewposition translated
+	float4x4 LocalToWorldScaleRotation;
+	double3 Translation;
+};
+
+ConstantBuffer<_ViewConstants>  ViewConstants : register(b0);
+ConstantBuffer<_DrawConstants>  DrawConstants : register(b1, space0);
+
+float4x4 GetLocalToWorldViewTranslated(in float4x4 InLTWSR, in double3 InTrans, in double3 InViewPosition)
+{
+	return float4x4(
+		InLTWSR[0][0], InLTWSR[0][1], InLTWSR[0][2], 0,
+		InLTWSR[1][0], InLTWSR[1][1], InLTWSR[1][2], 0,
+		InLTWSR[2][0], InLTWSR[2][1], InLTWSR[2][2], 0,
+		float3(InTrans - InViewPosition), 1);
+}
+
---
+++ Assets/shaders/debugSolidColor.hlsl
@@ -0,0 +1,35 @@
+#include "Common.hlsl"
+
+struct VertexShaderInput
+{
+	float3 position		: POSITION;
+	float3 color		: COLOR0;
+};
+
+struct PixelShaderInput
+{
+	float4 pixelPosition	: SV_POSITION;
+	float3 color			: COLOR0;
+};
+
+// Vertex shader
+[RootSignature(MESH_SIG)]
+PixelShaderInput main_vs(VertexShaderInput vin)
+{
+	PixelShaderInput vout;
+
+	float4x4 LocalToWorldTranslated = GetLocalToWorldViewTranslated(DrawConstants.LocalToWorldScaleRotation, DrawConstants.Translation, ViewConstants.ViewPosition);
+	float3 worldPosition = mul(float4(vin.position, 1.0), LocalToWorldTranslated).xyz;
+	vout.pixelPosition = mul(float4(worldPosition, 1.0), ViewConstants.ViewProjectionMatrix);
+	vout.color = vin.color;
+
+	return vout;
+}
+
+// Pixel shader
+[RootSignature(MESH_SIG)]
+float4 main_ps(PixelShaderInput pin) : SV_Target
+{	
+	// Final fragment color.
+	return float4(pin.color, 1.0);
+}
