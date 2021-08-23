d9e08c7e9e60d5008a7461160d0f5840c9d61b4c
David Sleeper <david.sleeper@gmail.com>
2021-08-23 09:49:41 -0400

    - little more cleanup

--- SPPAndroidBTTest/src/com/AndroidBTTest/AndroidBTTest.java
+++ SPPAndroidBTTest/src/com/AndroidBTTest/AndroidBTTest.java
@@ -22,6 +22,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.bluetooth.BluetoothClass;
 
 import android.view.View;
 import android.view.VelocityTracker;
@@ -30,36 +31,41 @@ import java.util.*;
 
 import android.view.MotionEvent;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+
 import org.json.*;
 
 public class AndroidBTTest extends Activity implements View.OnClickListener {
-	private BluetoothAdapter mAdapter = null;
+	
+	
 	private Spinner btChoices = null;
 	private Button doConnect = null;
 	
+	private BluetoothAdapter mAdapter = null;
 	private BluetoothSocket socketConnection = null;
 	private BluetoothClient connectedThread = null;
-
+	//MATCHED IN SPP ENGINE
 	private static final UUID BT_MODULE_UUID = UUID.fromString("263beec5-a7fe-443a-a9ee-9bdfc5fc17a3");
-
 	private static final String TAG = "BT";
-
 	
     private VelocityTracker mVelocityTracker = null;
-
 	private TextView tv = null;
-
-
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_main);
-
-		
+		super.onCreate(savedInstanceState);		
 
 		Log.d(TAG, "AndroidBTTest");
+
+		IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
+        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED);
+        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+        this.registerReceiver(_BTReceiver, filter);
 
 		LinearLayout layout = new LinearLayout(this);
 		layout.setOrientation(1);
@@ -72,16 +78,12 @@ public class AndroidBTTest extends Activity implements View.OnClickListener {
 		doConnect.setOnClickListener(this);
 
 		layout.addView(tv);
-
 		setContentView(layout);
-
 		btChoices = new Spinner(this);
 
 		// Spinner Drop down elements
 		List<String> BTNames = new ArrayList<String>();
-
 		mAdapter = BluetoothAdapter.getDefaultAdapter();
-
 		if (!mAdapter.isEnabled()) 
 		{
 			//Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -94,13 +96,16 @@ public class AndroidBTTest extends Activity implements View.OnClickListener {
 			// There are paired devices. Get the name and address of each paired device.
 			for (BluetoothDevice device : pairedDevices) 
 			{
-				String deviceName = device.getName();
-				String deviceHardwareAddress = device.getAddress(); // MAC address
-
-				BTNames.add(deviceName + ":" + deviceHardwareAddress);
-
-				Log.d(TAG, "Device Name: " + deviceName);
-				Log.d(TAG, "Device Addr: " + deviceHardwareAddress);
+				if( device.getBluetoothClass().getMajorDeviceClass() == BluetoothClass.Device.Major.COMPUTER )
+				{
+					String deviceName = device.getName();
+					String deviceHardwareAddress = device.getAddress(); // MAC address
+				
+					BTNames.add(deviceName + ":" + deviceHardwareAddress);
+					Log.d(TAG, "Device Name: " + deviceName);
+					Log.d(TAG, "Device Addr: " + deviceHardwareAddress);
+					Log.d(TAG, "Device Class ID: " + device.getBluetoothClass().getMajorDeviceClass());					
+				}
 			}
 
 			ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, 
@@ -116,6 +121,8 @@ public class AndroidBTTest extends Activity implements View.OnClickListener {
 		layout.addView(doConnect);
 	}
 
+
+
 	@Override
     public void onClick(View v)
     {
@@ -127,9 +134,46 @@ public class AndroidBTTest extends Activity implements View.OnClickListener {
 		Log.d(TAG, "CONNECT TO: " + text);
 		
 		final String address = text.substring(text.length() - 17);
-        BluetoothDevice device = mAdapter.getRemoteDevice(address);
+       
+		ConnectToDeviceByAddress(address);
+    }
 
-		Log.d(TAG, " - address: " + address);		
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getActionMasked();
+
+        switch(action) {
+            case MotionEvent.ACTION_DOWN:
+               
+				if(connectedThread!=null)
+				{
+					JSONObject obj = new JSONObject();
+					try 
+					{
+						obj.put("X", event.getX());
+						obj.put("Y", event.getY());
+					} 
+					catch (JSONException e)  {}					
+					connectedThread.write(obj.toString());
+				}
+
+                break;
+            case MotionEvent.ACTION_MOVE:   
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                break;
+        }
+
+		return true;
+	}
+
+	private void ConnectToDeviceByAddress(String InAddress)
+    {
+		Log.d(TAG, " - ConnectToDeviceByAddress: " + InAddress);		
+
+		BluetoothDevice device = mAdapter.getRemoteDevice(InAddress);
+
+		Log.d(TAG, " - address: " + InAddress);		
 		Log.d(TAG, " - GUID: " + BT_MODULE_UUID);		
 		
 		if(connectedThread != null)
@@ -171,39 +215,36 @@ public class AndroidBTTest extends Activity implements View.OnClickListener {
 
 		connectedThread = new BluetoothClient(socketConnection, null);
 		connectedThread.start();
-		Log.i(TAG, " - CONNECTING");
+		Log.d(TAG, " - CONNECTING");
     }
 
 	private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
 		return device.createRfcommSocketToServiceRecord(BT_MODULE_UUID);
 	}
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-        int action = event.getActionMasked();
+	//The BroadcastReceiver that listens for bluetooth broadcasts
+	private final BroadcastReceiver _BTReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+			String deviceName = device.getName();
 
-        switch(action) {
-            case MotionEvent.ACTION_DOWN:
-               
-				if(connectedThread!=null)
-				{
-					JSONObject obj = new JSONObject();
-					try 
-					{
-						obj.put("X", event.getX());
-						obj.put("Y", event.getY());
-					} 
-					catch (JSONException e)  {}					
-					connectedThread.write(obj.toString());
-				}
-
-                break;
-            case MotionEvent.ACTION_MOVE:   
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_CANCEL:
-                break;
-        }
-
-		return true;
-	}
+			if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+			  Log.d(TAG, "ACTION_FOUND BT: " + deviceName);
+			}
+			else if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+			   Log.d(TAG, "ACTION_ACL_CONNECTED BT: " + deviceName);
+			}
+			else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+			   Log.d(TAG, "ACTION_DISCOVERY_FINISHED BT: " + deviceName);
+			}
+			else if (BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED.equals(action)) {
+			   Log.d(TAG, "ACTION_ACL_DISCONNECT_REQUESTED BT: " + deviceName);
+			}
+			else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+			   Log.d(TAG, "ACTION_ACL_DISCONNECTED BT: " + deviceName);
+			}
+		}
+};
 }
