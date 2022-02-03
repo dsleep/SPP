72acd9302d6d5cafce79ca381f937ecd9670dbf1
David Sleeper <david.sleeper@gmail.com>
2022-02-03 09:15:45 -0500

    Mac updates

--- SPPCore/SPPHandledTimers.cpp
+++ SPPCore/SPPHandledTimers.cpp
@@ -1,9 +1,6 @@
 // Copyright (c) David Sleeper (Sleeping Robot LLC)
 // Distributed under MIT license, or public domain if desired and
 // recognized in your jurisdiction.
-
-#pragma once
-
 #include "SPPHandledTimers.h"
 
 namespace SPP
