83e353687cab665813f52308c5826579b1e9a084
David Sleeper <david.sleeper@gmail.com>
2022-03-31 08:25:10 -0400

    - c++20 updates

--- SPPCore/ThreadPool.h
+++ SPPCore/ThreadPool.h
@@ -50,9 +50,9 @@ namespace SPP
 		ThreadPool(uint8_t threads);		
 
 		template<class F, class... Args>
-		auto enqueue(F&& f, Args&&... args)->std::future<typename std::result_of<F(Args...)>::type>
+		auto enqueue(F&& f, Args&&... args)->std::future<typename std::invoke_result<F(Args...)>::type>
 		{
-			using return_type = typename std::result_of<F(Args...)>::type;
+			using return_type = typename std::invoke_result<F(Args...)>::type;
 
 			auto task = std::make_shared< std::packaged_task<return_type()> >(
 				std::bind(std::forward<F>(f), std::forward<Args>(args)...)
