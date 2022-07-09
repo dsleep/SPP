13602f40d5cb6974089063e27abee1dcd294856d
David Sleeper <david.sleeper@gmail.com>
2022-07-09 13:07:50 -0400

    - missing

---
+++ Assets/shaders/fullScreenColorWrite.hlsl
@@ -0,0 +1,29 @@
+// Copyright (c) David Sleeper (Sleeping Robot LLC)
+// Distributed under MIT license, or public domain if desired and
+// recognized in your jurisdiction.
+
+[[vk::binding(0, 0)]]
+Texture2D textureData : register(t0, space0);
+[[vk::binding(1, 0)]]
+SamplerState tSampler : register(s0);
+
+struct PixelShaderInput
+{
+	float4 pixelPosition	: SV_POSITION;
+	float2 texcoord			: TEXCOORD0;
+};
+
+PixelShaderInput main_vs(uint vI : SV_VERTEXID)
+{
+    PixelShaderInput results;
+	
+	results.texcoord = float2(vI&1,vI>>1); //you can use these for texture coordinates later		
+	results.pixelPosition = float4((results.texcoord.x-0.5f)*2, (results.texcoord.y-0.5f)*2, 0, 1);
+	
+	return results;
+}
+
+float4 main_ps(PixelShaderInput pin) :SV_TARGET
+{	
+	return textureData.Sample(tSampler, pin.texcoord);
+}
