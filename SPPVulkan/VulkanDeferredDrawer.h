2711c178466f361c1804b6e278fa352ddd3ea79b
David Sleeper <david.sleeper@gmail.com>
2022-09-08 10:37:14 -0400

    - deferred pass setup

---
+++ SPPVulkan/VulkanDeferredDrawer.h
@@ -0,0 +1,13 @@
+// Copyright (c) David Sleeper (Sleeping Robot LLC)
+// Distributed under MIT license, or public domain if desired and
+// recognized in your jurisdiction.
+
+#pragma once
+
+#include "SPPVulkan.h"
+#include "VulkanRenderScene.h"
+
+namespace SPP
+{
+	
+}
