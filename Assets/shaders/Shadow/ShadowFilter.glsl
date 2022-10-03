59c847477ad528ec361c960042f40b98f4976023
David Sleeper <david.sleeper@gmail.com>
2022-10-03 09:31:13 -0400

    - updates

---
+++ Assets/shaders/Shadow/ShadowFilter.glsl
@@ -0,0 +1,67 @@
+#version 450
+
+#extension GL_EXT_shader_16bit_storage: require
+#extension GL_EXT_shader_8bit_storage: require
+
+#extension GL_GOOGLE_include_directive: require
+
+//we are in fact using column
+layout(std430) buffer;
+
+#include "Common.glsl"
+
+layout (set = 2, binding = 0) uniform sampler2D shadowMap;
+
+layout (location = 0) in vec4 inPixelPosition;
+layout (location = 1) in vec2 inUV;
+
+layout (location = 0) out vec4 outputColor;
+
+float textureProj(vec4 shadowCoord, vec2 off)
+{
+	float shadow = 1.0;
+	if ( shadowCoord.z > -1.0 && shadowCoord.z < 1.0 ) 
+	{
+		float dist = texture( shadowMap, shadowCoord.st + off ).r;
+		if ( shadowCoord.w > 0.0 && dist < shadowCoord.z ) 
+		{
+			shadow = ambient;
+		}
+	}
+	return shadow;
+}
+
+float filterPCF(vec4 sc)
+{
+	ivec2 texDim = textureSize(shadowMap, 0);
+	float scale = 1.5;
+	float dx = scale * 1.0 / float(texDim.x);
+	float dy = scale * 1.0 / float(texDim.y);
+
+	float shadowFactor = 0.0;
+	int count = 0;
+	int range = 1;
+	
+	for (int x = -range; x <= range; x++)
+	{
+		for (int y = -range; y <= range; y++)
+		{
+			shadowFactor += textureProj(sc, vec2(dx*x, dy*y));
+			count++;
+		}
+	
+	}
+	return shadowFactor / count;
+}
+
+// pixel shader shader
+void main()
+{
+	vec3 cameraRay = normalize(Multiply(vec4(inPixelPosition.xy, 1, 1.0), ViewConstants.InvViewProjectionMatrix).xyz);	
+
+	float shadow = (enablePCF == 1) ? filterPCF(inShadowCoord / inShadowCoord.w) : textureProj(inShadowCoord / inShadowCoord.w, vec2(0.0));
+
+	
+	outputColor = vec4(texture(samplerSky, cameraRay).rgb,1.0f);
+}
+
