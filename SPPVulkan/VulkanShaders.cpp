982d83987df44929147ce75b95e7eecf65818c71
David Sleeper <david.sleeper@gmail.com>
2023-02-25 16:19:52 -0500

    - various logging updates

--- SPPVulkan/VulkanShaders.cpp
+++ SPPVulkan/VulkanShaders.cpp
@@ -258,7 +258,7 @@ namespace SPP
 		return Storage;
 	}
 
-	static bool const GUseShaderCached = false;
+	static bool const GUseShaderCached = true;
 	static uint32_t const ShaderCacheVersion = 2;
 
 	bool VulkanShader::CompileShaderFromString(const std::string& ShaderSource, const AssetPath& ReferencePath, const char* EntryPoint, std::string* oErrorMsgs) 
@@ -388,7 +388,17 @@ namespace SPP
 			assert(result == SPV_REFLECT_RESULT_SUCCESS);
 
 			SpvReflectShaderModule module = spvReflSM.GetShaderModule();
-			WriteReflection(spvReflSM, false, std::cout);
+			{
+				std::ostringstream refStream;
+				WriteReflection(spvReflSM, false, refStream);
+				std::string refStr = refStream.str();
+				auto writeRefStrPath = stdfs::path(GetLogPath()).parent_path() / stdfs::path(*ReferencePath).filename();
+				writeRefStrPath.replace_extension(".refl.txt");
+				
+				SPP_LOG(LOG_VULKANSHADER, LOG_INFO, " - writing refl data %s", writeRefStrPath.generic_string().c_str());
+
+				WriteStringToFile(writeRefStrPath.generic_string().c_str(), refStr);
+			}
 
 			uint32_t count = 0;
 
@@ -401,10 +411,10 @@ namespace SPP
 			result = spvReflectEnumeratePushConstantBlocks(&module, &count, pushConstants.data());
 			assert(result == SPV_REFLECT_RESULT_SUCCESS);
 
-			SPP_LOG(LOG_VULKANSHADER, LOG_INFO, "PUSH CONSTANTS");
+			SPP_LOG(LOG_VULKANSHADER, LOG_VERBOSE, "PUSH CONSTANTS");
 			for (auto& pushvars : pushConstants)
 			{
-				SPP_LOG(LOG_VULKANSHADER, LOG_INFO, " - %s %d %d", pushvars->name, pushvars->offset, pushvars->size);
+				SPP_LOG(LOG_VULKANSHADER, LOG_VERBOSE, " - %s %d %d", pushvars->name, pushvars->offset, pushvars->size);
 
 				_pushConstants.push_back(
 					VkPushConstantRange{
@@ -422,47 +432,47 @@ namespace SPP
 			result = spvReflectEnumerateDescriptorSets(&module, &count, sets.data());
 			assert(result == SPV_REFLECT_RESULT_SUCCESS);
 
-			{
-				uint32_t var_count = 0;
-				result = spvReflectEnumerateInputVariables(&module, &var_count, NULL);
-				assert(result == SPV_REFLECT_RESULT_SUCCESS);
-				if (var_count > 0)
-				{
-					SpvReflectInterfaceVariable** input_vars =
-						(SpvReflectInterfaceVariable**)malloc(var_count * sizeof(SpvReflectInterfaceVariable*));
-					result = spvReflectEnumerateInputVariables(&module, &var_count, input_vars);
-					assert(result == SPV_REFLECT_RESULT_SUCCESS);
+			//{
+			//	uint32_t var_count = 0;
+			//	result = spvReflectEnumerateInputVariables(&module, &var_count, NULL);
+			//	assert(result == SPV_REFLECT_RESULT_SUCCESS);
+			//	if (var_count > 0)
+			//	{
+			//		SpvReflectInterfaceVariable** input_vars =
+			//			(SpvReflectInterfaceVariable**)malloc(var_count * sizeof(SpvReflectInterfaceVariable*));
+			//		result = spvReflectEnumerateInputVariables(&module, &var_count, input_vars);
+			//		assert(result == SPV_REFLECT_RESULT_SUCCESS);
 
-					SPP_LOG(LOG_VULKANSHADER, LOG_INFO, "INPUT VARIABLES");
+			//		SPP_LOG(LOG_VULKANSHADER, LOG_INFO, "INPUT VARIABLES");
 
-					for (uint32_t Iter = 0; Iter < var_count; Iter++)
-					{
-						SPP_LOG(LOG_VULKANSHADER, LOG_INFO, " - %s : %d", input_vars[Iter]->name, input_vars[Iter]->location);
-					}
-					free(input_vars);
-				}
-			}
+			//		for (uint32_t Iter = 0; Iter < var_count; Iter++)
+			//		{
+			//			SPP_LOG(LOG_VULKANSHADER, LOG_INFO, " - %s : %d", input_vars[Iter]->name, input_vars[Iter]->location);
+			//		}
+			//		free(input_vars);
+			//	}
+			//}
 
-			{
-				uint32_t var_count = 0;
-				result = spvReflectEnumerateOutputVariables(&module, &var_count, NULL);
-				assert(result == SPV_REFLECT_RESULT_SUCCESS);
-				if (var_count > 0)
-				{
-					SpvReflectInterfaceVariable** reflected_vars =
-						(SpvReflectInterfaceVariable**)malloc(var_count * sizeof(SpvReflectInterfaceVariable*));
-					result = spvReflectEnumerateOutputVariables(&module, &var_count, reflected_vars);
-					assert(result == SPV_REFLECT_RESULT_SUCCESS);
+			//{
+			//	uint32_t var_count = 0;
+			//	result = spvReflectEnumerateOutputVariables(&module, &var_count, NULL);
+			//	assert(result == SPV_REFLECT_RESULT_SUCCESS);
+			//	if (var_count > 0)
+			//	{
+			//		SpvReflectInterfaceVariable** reflected_vars =
+			//			(SpvReflectInterfaceVariable**)malloc(var_count * sizeof(SpvReflectInterfaceVariable*));
+			//		result = spvReflectEnumerateOutputVariables(&module, &var_count, reflected_vars);
+			//		assert(result == SPV_REFLECT_RESULT_SUCCESS);
 
-					SPP_LOG(LOG_VULKANSHADER, LOG_INFO, "OUTPUT VARIABLES");
+			//		SPP_LOG(LOG_VULKANSHADER, LOG_INFO, "OUTPUT VARIABLES");
 
-					for (uint32_t Iter = 0; Iter < var_count; Iter++)
-					{
-						SPP_LOG(LOG_VULKANSHADER, LOG_INFO, " - %s : %d", reflected_vars[Iter]->name, reflected_vars[Iter]->location);
-					}
-					free(reflected_vars);
-				}
-			}
+			//		for (uint32_t Iter = 0; Iter < var_count; Iter++)
+			//		{
+			//			SPP_LOG(LOG_VULKANSHADER, LOG_INFO, " - %s : %d", reflected_vars[Iter]->name, reflected_vars[Iter]->location);
+			//		}
+			//		free(reflected_vars);
+			//	}
+			//}
 
 			//std::vector<DescriptorSetLayoutData> set_layouts(sets.size(), DescriptorSetLayoutData{});
 
@@ -514,23 +524,23 @@ namespace SPP
 				//layout.create_info.pBindings = layout.bindings.data();
 			}
 
-			const char* tt = "  ";
-			PrintModuleInfo(module, "");
-			SPP_LOG(LOG_VULKANSHADER, LOG_INFO, "Descriptor sets:");
-			for (size_t index = 0; index < sets.size(); ++index) {
-				auto p_set = sets[index];
+			//const char* tt = "  ";
+			//PrintModuleInfo(module, "");
+			//SPP_LOG(LOG_VULKANSHADER, LOG_INFO, "Descriptor sets:");
+			//for (size_t index = 0; index < sets.size(); ++index) {
+			//	auto p_set = sets[index];
 
-				// descriptor sets can also be retrieved directly from the module, by set index
-				auto p_set2 = spvReflectGetDescriptorSet(&module, p_set->set, &result);
-				assert(result == SPV_REFLECT_RESULT_SUCCESS);
-				assert(p_set == p_set2);
-				(void)p_set2;
+			//	// descriptor sets can also be retrieved directly from the module, by set index
+			//	auto p_set2 = spvReflectGetDescriptorSet(&module, p_set->set, &result);
+			//	assert(result == SPV_REFLECT_RESULT_SUCCESS);
+			//	assert(p_set == p_set2);
+			//	(void)p_set2;
 
-				SPP_LOG(LOG_VULKANSHADER, LOG_INFO, "%sSET : GPU IDX %d", tt, p_set->set);
-				PrintDescriptorSet(*p_set, tt);
-			}
+			//	SPP_LOG(LOG_VULKANSHADER, LOG_INFO, "%sSET : GPU IDX %d", tt, p_set->set);
+			//	PrintDescriptorSet(*p_set, tt);
+			//}
 
-			spvReflectDestroyShaderModule(&module);
+			//spvReflectDestroyShaderModule(&module);
 		}
 
 		//CACHING
