6c66f35064168615fd7eb5c497fe738b6e9e743e
David Sleeper <david@studiowildcard.com>
2024-02-08 10:17:59 -0500

    - more android updates

--- AndroidTipsApp/app/src/main/java/com/sleepingrobot/androidtipsapp/MainActivity.java
+++ AndroidTipsApp/app/src/main/java/com/sleepingrobot/androidtipsapp/MainActivity.java
@@ -21,28 +21,54 @@ import android.os.Bundle;
 import android.util.Log;
 import java.io.*;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
+
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattServer;
+import android.bluetooth.BluetoothGattServerCallback;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import android.os.ParcelUuid;
+
 import android.view.View;
 import android.view.VelocityTracker;
 import java.util.*;
 import android.view.MotionEvent;
 import org.json.*;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private BluetoothAdapter mAdapter = null;
-    private Spinner btChoices = null;
-    private Button doConnect = null;
-    private BluetoothSocket socketConnection = null;
-    private BluetoothClient connectedThread = null;
+public class MainActivity extends AppCompatActivity {
     private static final UUID BT_MODULE_UUID = UUID.fromString("263beec5-a7fe-443a-a9ee-9bdfc5fc17a3");
     private static final String TAG = "BT";
-    private VelocityTracker mVelocityTracker = null;
+    // private VelocityTracker mVelocityTracker = null;
+
+    private BluetoothManager mBluetoothManager;
+    private BluetoothGattServer mBluetoothGattServer = null;
+    private BluetoothLeAdvertiser mBluetoothLeAdvertiser;
+
+    /* Collection of notification subscribers */
+    private Set<BluetoothDevice> mRegisteredDevices = new HashSet<>();
 
     private TextView tv = null;
+
+    private String lastPosition = "";
 
     public void checkPermission(String permission, int requestCode)
     {
@@ -60,6 +86,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions,
                                            int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+
         switch (requestCode) {
             case 1:
                 // If request is cancelled, the result arrays are empty.
@@ -89,9 +117,217 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //setContentView(R.layout.activity_main);
         checkPermission("android.permission.BLUETOOTH_CONNECT", 1);
+    }
+
+    private BluetoothGattService CreateGattService()
+    {
+        BluetoothGattService service = new BluetoothGattService(BT_MODULE_UUID,
+                BluetoothGattService.SERVICE_TYPE_PRIMARY);
+
+        // Current Time characteristic
+        BluetoothGattCharacteristic currentTime = new BluetoothGattCharacteristic(BT_MODULE_UUID,
+                //Read-only characteristic, supports notifications
+                BluetoothGattCharacteristic.PERMISSION_WRITE,
+                BluetoothGattCharacteristic.PERMISSION_READ);
+
+        service.addCharacteristic(currentTime);
+        return service;
+    }
+
+    private BluetoothGattServerCallback mGattServerCallback = new BluetoothGattServerCallback() {
+
+        @Override
+        public void onConnectionStateChange(BluetoothDevice device, int status, int newState) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                Log.i(TAG, "BluetoothDevice CONNECTED: " + device);
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                Log.i(TAG, "BluetoothDevice DISCONNECTED: " + device);
+                //Remove device from any active subscriptions
+                mRegisteredDevices.remove(device);
+            }
+        }
+
+        @Override
+        public void onCharacteristicReadRequest(BluetoothDevice device, int requestId, int offset,
+                                                BluetoothGattCharacteristic characteristic) {
+            if (BT_MODULE_UUID.equals(characteristic.getUuid())) {
+                Log.i(TAG, "Read CurrentTime");
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_SUCCESS,
+                        0,
+                        lastPosition.getBytes());
+            } else {
+                // Invalid characteristic
+                Log.w(TAG, "Invalid Characteristic Read: " + characteristic.getUuid());
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_FAILURE,
+                        0,
+                        null);
+            }
+        }
+
+        @Override
+        public void onDescriptorReadRequest(BluetoothDevice device, int requestId, int offset,
+                                            BluetoothGattDescriptor descriptor) {
+            if (BT_MODULE_UUID.equals(descriptor.getUuid())) {
+                Log.d(TAG, "Config descriptor read");
+                byte[] returnValue;
+                if (mRegisteredDevices.contains(device)) {
+                    returnValue = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE;
+                } else {
+                    returnValue = BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE;
+                }
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_FAILURE,
+                        0,
+                        returnValue);
+            } else {
+                Log.w(TAG, "Unknown descriptor read request");
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_FAILURE,
+                        0,
+                        null);
+            }
+        }
+
+        @Override
+        public void onDescriptorWriteRequest(BluetoothDevice device, int requestId,
+                                             BluetoothGattDescriptor descriptor,
+                                             boolean preparedWrite, boolean responseNeeded,
+                                             int offset, byte[] value) {
+            if (BT_MODULE_UUID.equals(descriptor.getUuid())) {
+                if (Arrays.equals(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE, value)) {
+                    Log.d(TAG, "Subscribe device to notifications: " + device);
+                    mRegisteredDevices.add(device);
+                } else if (Arrays.equals(BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE, value)) {
+                    Log.d(TAG, "Unsubscribe device from notifications: " + device);
+                    mRegisteredDevices.remove(device);
+                }
+
+                if (responseNeeded) {
+                    mBluetoothGattServer.sendResponse(device,
+                            requestId,
+                            BluetoothGatt.GATT_SUCCESS,
+                            0,
+                            null);
+                }
+            } else {
+                Log.w(TAG, "Unknown descriptor write request");
+                if (responseNeeded) {
+                    mBluetoothGattServer.sendResponse(device,
+                            requestId,
+                            BluetoothGatt.GATT_FAILURE,
+                            0,
+                            null);
+                }
+            }
+        }
+    };
+
+    private void startServer() {
+        mBluetoothGattServer = mBluetoothManager.openGattServer(this, mGattServerCallback);
+        if (mBluetoothGattServer == null) {
+            Log.w(TAG, "Unable to create GATT server");
+            return;
+        }
+
+        mBluetoothGattServer.addService(CreateGattService());
+    }
+
+    /**
+     * Shut down the GATT server.
+     */
+    private void stopServer() {
+        if (mBluetoothGattServer == null) return;
+
+        mBluetoothGattServer.close();
+        mBluetoothGattServer = null;
+    }
+
+    private BroadcastReceiver mBluetoothReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.STATE_OFF);
+
+            switch (state) {
+                case BluetoothAdapter.STATE_ON:
+                    startAdvertising();
+                    startServer();
+                    break;
+                case BluetoothAdapter.STATE_OFF:
+                    stopServer();
+                    stopAdvertising();
+                    break;
+                default:
+                    // Do nothing
+            }
+
+        }
+    };
 
 
+    private AdvertiseCallback mAdvertiseCallback = new AdvertiseCallback() {
+        @Override
+        public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+            Log.i(TAG, "LE Advertise Started.");
+        }
 
+        @Override
+        public void onStartFailure(int errorCode) {
+            Log.w(TAG, "LE Advertise Failed: "+errorCode);
+        }
+    };
+
+    private void startAdvertising()
+    {
+        BluetoothAdapter bluetoothAdapter = mBluetoothManager.getAdapter();
+        mBluetoothLeAdvertiser = bluetoothAdapter.getBluetoothLeAdvertiser();
+        if (mBluetoothLeAdvertiser == null) {
+            Log.w(TAG, "Failed to create advertiser");
+            return;
+        }
+
+        AdvertiseSettings settings = new AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
+                .setConnectable(true)
+                .setTimeout(0)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+                .build();
+
+        AdvertiseData data = new AdvertiseData.Builder()
+                .setIncludeDeviceName(true)
+                .setIncludeTxPowerLevel(false)
+                .addServiceUuid(new ParcelUuid(BT_MODULE_UUID))
+                .build();
+
+        mBluetoothLeAdvertiser
+                .startAdvertising(settings, data, mAdvertiseCallback);
+    }
+
+    private void stopAdvertising() {
+        if (mBluetoothLeAdvertiser == null) return;
+
+        mBluetoothLeAdvertiser.stopAdvertising(mAdvertiseCallback);
+    }
+
+    private void notifyRegisteredDevices() {
+        if (mRegisteredDevices.isEmpty()) {
+            //Log.i(TAG, "No subscribers registered");
+            return;
+        }
+
+        Log.i(TAG, "Sending update to " + mRegisteredDevices.size() + " subscribers");
+        for (BluetoothDevice device : mRegisteredDevices) {
+            BluetoothGattCharacteristic positionValue = mBluetoothGattServer
+                    .getService(BT_MODULE_UUID)
+                    .getCharacteristic(BT_MODULE_UUID);
+            positionValue.setValue(lastPosition);
+            mBluetoothGattServer.notifyCharacteristicChanged(device, positionValue, false);
+        }
     }
 
     private void createScene()
@@ -102,115 +338,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tv = new TextView(this);
         tv.setText("BT TEST!!");
 
-        doConnect = new Button(this);
-        doConnect.setText("Connect");
-        doConnect.setOnClickListener(this);
-
         layout.addView(tv);
 
         setContentView(layout);
-
-        btChoices = new Spinner(this);
-
-        // Spinner Drop down elements
-        List<String> BTNames = new ArrayList<String>();
-
-        mAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        if (!mAdapter.isEnabled())
-        {
-            //Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            //startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-        else
-        {
-            Set<BluetoothDevice> pairedDevices = mAdapter.getBondedDevices();
-
-            // There are paired devices. Get the name and address of each paired device.
-            for (BluetoothDevice device : pairedDevices)
-            {
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-
-                BTNames.add(deviceName + ":" + deviceHardwareAddress);
-
-                Log.d(TAG, "Device Name: " + deviceName);
-                Log.d(TAG, "Device Addr: " + deviceHardwareAddress);
-            }
-
-            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                    android.R.layout.simple_spinner_item,
-                    BTNames);
-
-            // attaching data adapter to spinner
-            btChoices.setAdapter(dataAdapter);
-
-            layout.addView(btChoices);
-        }
-
-        layout.addView(doConnect);
-    }
-
-    @Override
-    public void onClick(View v)
-    {
-        String text = btChoices.getSelectedItem().toString();
-
-        Log.d(TAG, "************************");
-        Log.d(TAG, "************************");
-        Log.d(TAG, "************************");
-        Log.d(TAG, "CONNECT TO: " + text);
-
-        final String address = text.substring(text.length() - 17);
-        BluetoothDevice device = mAdapter.getRemoteDevice(address);
-
-        Log.d(TAG, " - address: " + address);
-        Log.d(TAG, " - GUID: " + BT_MODULE_UUID);
-
-        if(connectedThread != null)
-        {
-            connectedThread.stop();
-            connectedThread = null;
-        }
-        if(socketConnection != null)
-        {
-            try
-            {
-                socketConnection.close();
-                socketConnection = null;
-            } catch (IOException e) {
-            }
-        }
-
-        try {
-            socketConnection = createBluetoothSocket(device);
-            Log.d(TAG, " - created socket: " + socketConnection);
-        } catch (IOException e) {
-            Log.d(TAG, " - SOCKET FAILED");
-            return;
-        }
-        try {
-            Log.d(TAG, " - called connect: " + socketConnection);
-            socketConnection.connect();
-        } catch (IOException e) {
-            Log.d(TAG, " - CONNECT FAILED");
-            try {
-                socketConnection.close();
-            } catch (IOException e2) {
-                //insert code to deal with this
-            }
-            return;
-        }
-
-        tv.setText("CONNECTED");
-
-        connectedThread = new BluetoothClient(socketConnection, null);
-        connectedThread.start();
-        Log.i(TAG, " - CONNECTING");
-    }
-
-    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
-        return device.createRfcommSocketToServiceRecord(BT_MODULE_UUID);
     }
 
     @Override
@@ -220,7 +350,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch(action) {
             case MotionEvent.ACTION_DOWN:
 
-                if(connectedThread!=null)
+                //if(connectedThread!=null)
                 {
                     JSONObject obj = new JSONObject();
                     try
@@ -229,7 +359,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         obj.put("Y", event.getY());
                     }
                     catch (JSONException e)  {}
-                    connectedThread.write(obj.toString());
+
+                    lastPosition = obj.toString();
+                    notifyRegisteredDevices();
+                    //connectedThread.write(obj.toString());
                 }
 
                 break;
